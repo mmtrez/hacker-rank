515dfa4b4cb9010244054cae0cbf8d56ca128cde
Mohammad Seydizadeh <immtrez@gmail.com>
2023-08-30 09:57:54 +0330

    mini max sum

---
+++ challenges/miniMaxSum.ts
@@ -0,0 +1,31 @@
+/*
+  Complete the 'miniMaxSum' function below.
+  The function accepts INTEGER_ARRAY arr as parameter.
+  https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+*/
+
+type MiniMaxSum = (val: number[]) => void;
+
+const miniMaxSum: MiniMaxSum = (arr) => {
+  const length = arr.length;
+  const sortedArr = arr.sort((a, b) => a - b);
+  let min = 0;
+  let max = 0;
+
+  sortedArr.forEach((value, index) => {
+    if (index === 0) {
+      min += value;
+    } else if (index === length - 1) {
+      max += value;
+    } else {
+      min += value;
+      max += value;
+    }
+  });
+
+  console.log(`${min} ${max}`);
+};
+
+// Test
+
+miniMaxSum([1, 2, 3, 4, 5]);
