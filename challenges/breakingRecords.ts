a5b102f60e2b43c5819fb44f255fb8e8aad66514
Mohammad Seydizadeh <immtrez@gmail.com>
2023-08-30 09:57:02 +0330

    breaking records

---
+++ challenges/breakingRecords.ts
@@ -0,0 +1,22 @@
+type BreakingRecords = (val: number[]) => number[];
+const breakingRecords: BreakingRecords = (scores) => {
+  const output = [0, 0];
+  let highest = scores[0];
+  let lowest = scores[0];
+
+  scores.forEach((score) => {
+    if (score > highest) {
+      output[0]++;
+      highest = score;
+    }
+    if (score < lowest) {
+      output[1]++;
+      lowest = score;
+    }
+  });
+
+  return output;
+};
+
+// Test
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
