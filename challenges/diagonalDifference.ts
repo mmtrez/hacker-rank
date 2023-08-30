8c416659c514d5cbaea530b4e4e4593515b40972
Mohammad Seydizadeh <immtrez@gmail.com>
2023-08-30 09:57:24 +0330

    diagonal diffrence

---
+++ challenges/diagonalDifference.ts
@@ -0,0 +1,30 @@
+/*
+  Complete the 'diagonalDifference' function below.
+  The function is expected to return an INTEGER.
+  The function accepts 2D_INTEGER_ARRAY arr as parameter.
+  https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true 
+*/
+
+type DiagonalDifference = (val: number[][]) => number;
+
+const diagonalDifference: DiagonalDifference = (arr) => {
+  const zeroBasedlength = arr.length - 1;
+  let rtlDiagonal = 0;
+  let ltrDiagonal = 0;
+
+  for (let i = 0; i <= zeroBasedlength; i++) {
+    rtlDiagonal += arr[i][i];
+    ltrDiagonal += arr[i][zeroBasedlength - i];
+  }
+
+  return Math.abs(ltrDiagonal - rtlDiagonal);
+};
+
+// Test
+console.log(
+  diagonalDifference([
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12],
+  ]),
+);
